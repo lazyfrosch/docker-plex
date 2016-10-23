aa50e2541bbedfb536593678f8635e0066bb83df
Markus Frosch <markus@lazyfrosch.de>
2016-10-23 17:21:12 +0200

    Remove any stale PID from data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# remove old pid file
+rm -f '/plex/Plex Media Server/plexmediaserver.pid'
+
 if [ "$1" = "plex" ]; then
     chown -R plex.plex /plex
     exec start-stop-daemon --start -c plex --exec /usr/local/bin/plex
